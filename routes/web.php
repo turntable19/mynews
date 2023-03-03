08aa7707bb5621fbb22d71c9d54faabeac9dd1b0
turntable19 <minatomina811@gmail.com>
2023-03-03 15:20:11 +0900

    second

--- routes/web.php
+++ routes/web.php
@@ -31,3 +31,6 @@ Route::controller(ProfileController::class)->prefix('admin')->group(function() {
 Route::controller(NewsController::class)->prefix('admin')->group(function() {
     Route::get('news/create', 'add');
 });
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
--- webpack.mix.js
+++ webpack.mix.js
@@ -11,7 +11,8 @@ const mix = require('laravel-mix');
  |
  */
 
-mix.js('resources/js/app.js', 'public/js')
-    .postCss('resources/css/app.css', 'public/css', [
-        //
-    ]);
+ mix.js('resources/js/app.js', 'public/js')
+ .sass('resources/sass/app.scss', 'public/css')
+ .sass('resources/sass/admin.scss', 'public/css')
+ .sass('resources/sass/profile.scss', 'public/css')
+ .sourceMaps();
